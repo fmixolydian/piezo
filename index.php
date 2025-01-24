c65d5d52ec28d0a0693f20bad2af922e14bf9118
fmixolydian <fmixolydian@proton.me>
2025-01-24 19:13:35 +0100

    testing

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-echo impole(" ", $_SERVER);
+require __DIR__."/config.php";
+
+//if there is no query string, redirect
+if (!array_key_exists("QUERY_STRING", $_SERVER)) {
+	header("Location: ".$PIEZO_SITE.$PIEZO_ROOT."index.php?list");
+	die();
+}
+
+echo var_dump($_SERVER);
+echo var_dump($argv);
+echo var_dump($_SERVER['argv']);
+echo __DIR__;
 
 ?>
